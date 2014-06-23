fbb898d406b3b607e49f29cbee63b66b0c78cc07
Grayson Wright <wright.grayson@gmail.com>
2014-06-23 01:42:52 -0400

    Update map with locations from address fields

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,4 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  actions: {
+             updateMap: function() {
+                          var routeA = this.get('routeA');
+                          var origin = routeA.get('origin').get('address');
+                          var destination = routeA.get('destination').get('address');
+                          displayRoute(directionsDisplayA, origin, destination);
+
+                          var routeB = this.get('routeB');
+                          var origin = routeB.get('origin').get('address');
+                          var destination = routeB.get('destination').get('address');
+                          displayRoute(directionsDisplayB, origin, destination);
+                        }
+           }
 });
--- app/models/location.js
+++ app/models/location.js
@@ -4,9 +4,5 @@ export default DS.Model.extend({
   latitude: DS.attr('number'),
        longitude: DS.attr('number'),
 
-       address: function() {
-         var latitude = this.get('latitude');
-         var longitude = this.get('longitude');
-         return '('+latitude+','+longitude+')';
-       }.property('latitude', 'longitude')
+       address: DS.attr('string')
 });
